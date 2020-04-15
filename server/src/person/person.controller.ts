244e72dcdbe1839e4344d5d324b5992c31ced0d4
manuel.flores.yanez@gmail.com <manuel.flores.yanez@gmail.com>
2020-04-14 21:53:18 -0400

    Starting to create Second Request

--- server/src/person/person.controller.ts
+++ server/src/person/person.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Res, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Res, HttpStatus, Post, Body } from '@nestjs/common';
 
 @Controller('people')
 export class PersonController {
@@ -10,4 +10,14 @@ export class PersonController {
             message: 'Success'
         })
     }
+
+    @Post()
+    CreatePerson(@Res() res,@Body() body){
+        return res.status(HttpStatus.OK).json({
+            message: 'Person created'
+        })
+    }
+
+
+
 }
