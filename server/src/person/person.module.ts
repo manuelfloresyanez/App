a3eebb3c3a1ca501742e2e8dabc0124c2fc7563b
manuelfloresyanez <manuel.flores.yanez@gmail.com>
2020-04-14 22:55:16 -0400

    Fix constructor

--- server/src/person/person.module.ts
+++ server/src/person/person.module.ts
@@ -3,6 +3,7 @@ import { PersonController } from './person.controller';
 import { PersonService } from './person.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PersonSchema } from './schemas/person.schemas';
+import { Model } from 'mongoose';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'Person', schema: PersonSchema}])],
--- server/src/person/person.service.ts
+++ server/src/person/person.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class PersonService {
 
+    constructor(@InjectModel('Person') private readonly personModel: Model<any>){
+
+    }
+
     ViewAllPeople(){
+        
         return {rut: 'success'}
     }
 
